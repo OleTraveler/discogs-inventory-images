01c1f3522eb6cc14774e42820a9dde3db02e3165
Travis Stevens <Travis.Stevens@gaiam.com>
2016-12-16 09:48:28 -0500

    working on buy and sell pages

--- sell.js
+++ sell.js
@@ -1,6 +1,3 @@
-
-$('table.sell > thead > tr').append("<th>IMG</th>");
-
 var dbx = new Dropbox({ accessToken: accessToken });
 
 function filePath(itemNumber) {
@@ -25,6 +22,7 @@ function uploadToDropbox(file, itemNumber) {
 function initUpload(dragToElement, itemNumber) {
   dragToElement.style="border:solid";
   dragToElement.ondragenter = dragToElement.ondragover = function (e) {
+    console.log("I");
     e.preventDefault();
     e.dataTransfer.dropEffect = 'copy';
     e.style="border-color:green";
@@ -34,6 +32,7 @@ function initUpload(dragToElement, itemNumber) {
 
   dragToElement.ondrop = function (e) {
     e.preventDefault();
+    console.log("ondrop");
     var funcItemNumber = itemNumber;
     // e.dataTransfer is a DataTransfer object (https://developer.mozilla.org/En/DragDrop/DataTransfer), e.dataTransfer.files is a FileList object (https://developer.mozilla.org/en/DOM/FileList)
     for (var i = 0; i < e.dataTransfer.files.length; i++) {
@@ -45,21 +44,8 @@ function initUpload(dragToElement, itemNumber) {
   }
 }
 
-function addImage(tr) {
-  var itemNumber = $(tr).children('td:nth-child(2)').text()
-  if (itemNumber !== null) {
-    itemNumber = itemNumber.trim();
-    var html = '<div id="uploadItem' + itemNumber + '" style="width: 100%; border: 1px solid blue">Drop here</div>'
-
-    $(tr).children('td:last').html(html);
-    initUpload(document.getElementById('uploadItem' + itemNumber), itemNumber);
-
-  }
-}
-
-
 function updateUploadHtml(itemNumber) {
-  var element = document.getElementById('uploadItem' + itemNumber);
+  var element = document.getElementById('draghere');
   if (element) {
     var path = filePath(itemNumber);
     dbx.filesListFolder({path: path})
@@ -74,45 +60,23 @@ function updateUploadHtml(itemNumber) {
       })
       .catch(function(error) {
         console.error(error);
-      }); 
+      });
   }
 
 }
 
-var tr = $('table.sell > tbody > tr').append('<td>') ;
+var aside = $(".marketplace_aside");
 
-for (var i = 0; i < tr.length; i++) {
-  addImage(tr[i]);
-}
+var itemNumber = window.location.href.substr(window.location.href.lastIndexOf('/') + 1);
 
+console.log("itemNumber:" + itemNumber);
 
+aside.append('<div style="border:solid" class="section" id="draghere">Drag Here</div>');
 
-function initialFileList() {
-  dbx.filesListFolder({path: ''})
-    .then(function(response) {
-      for (var i = 0; i < response.entries.length; i++) {
-        updateUploadHtml(response.entries[i].name);
-      }
-    })
-    .catch(function(error) {
-      console.error(error);
-    });  
-}
+var drag = document.getElementById("draghere");
 
-initialFileList();
+console.log("drag:" + drag);
 
+initUpload(drag, itemNumber);
 
-function uploadFile(itemNumber, form) {
-  var dbx = new Dropbox({ accessToken: accessToken, clientId: CLIENT_ID });
-
-  var fileInput = form.getElementByClassName('file-upload');
-  var file = fileInput.files[0];
-
-  dbx.filesUpload({path: '/' + itemNumber + '/' + file.name, contents: file})
-    .then(function(response) {
-    })
-    .catch(function(error) {
-      console.error('error' + error);
-    });
-  return false;
-}
+updateUploadHtml(itemNumber);
